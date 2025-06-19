6f9718bda4524a3704ccaa9ee945d361c5a65040
danielz0102 <danyzglez@hotmail.com>
2025-06-19 16:01:16 -0600

    feat: add middleware to handle 404 errors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { rootRouter } from './routers/rootRouter.js'
 import { handleError } from '#middlewares/handleError.js'
+import { render404 } from '#middlewares/render404.js'
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.set('views', './views')
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/', rootRouter)
+app.use(render404)
 app.use(handleError)
 
 app.listen(3000, () => {
